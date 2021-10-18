74dd8dabd55ed382e3446e62dac014c5d66aa3d2
Chris Mottram <C.Mottram@ljmu.ac.uk>
2021-10-18 16:50:54 +0100

    More implementation added to Wms_Qli50_Server_Loop. Extra callbacks API added for PAR and STA commands.

--- qli50/c/wms_qli50_server.c
+++ qli50/c/wms_qli50_server.c
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "log_udp.h"
+#include "wms_serial_general.h"
 #include "wms_serial_serial.h"
 #include "wms_qli50_command.h"
 #include "wms_qli50_connection.h"
@@ -34,12 +35,16 @@
  * <dl>
  * <dt>Read_Sensor_Callback</dt> <dd>The callback the server invokes when it receives a 'Read Sensor' request.</dd>
  * <dt>Send_Result_Callback</dt> <dd>The callback the server invokes when it receives a 'Send Result' request.</dd>
+ * <dt>Par_Callback</dt> <dd>The callback the server invokes when it receives a 'PAR' request.</dd>
+ * <dt>Sta_Callback</dt> <dd>The callback the server invokes when it receives a 'STA' request.</dd>
  * </dl>
  */
 struct Server_Struct
 {
 	Read_Sensor_Callback_T Read_Sensor_Callback;
 	Send_Result_Callback_T Send_Result_Callback;
+	Par_Callback_T Par_Callback;
+	Sta_Callback_T Sta_Callback;
 };
 
 /* internal data */
@@ -87,6 +92,36 @@ int Wms_Qli50_Server_Set_Send_Result_Callback(char *class,char *source,Send_Resu
 }
 
 /**
+ * Routine to set the callback to be invoked by the server when it receives a 'STA' request.
+ * @param class The class parameter for logging.
+ * @param source The source parameter for logging.
+ * @param callback A function pointer of type Sta_Callback_T.
+ * @return The procedure returns TRUE if successful, and FALSE if it failed 
+ *         (Wms_Qli50_Error_Number and Wms_Qli50_Error_String are filled in on failure).
+ * @see #Server_Data
+ */
+int Wms_Qli50_Server_Set_Sta_Callback(char *class,char *source,Sta_Callback_T callback)
+{
+	Server_Data.Sta_Callback = callback;
+	return TRUE;
+}
+
+/**
+ * Routine to set the callback to be invoked by the server when it receives a 'PAR' request.
+ * @param class The class parameter for logging.
+ * @param source The source parameter for logging.
+ * @param callback A function pointer of type Par_Callback_T.
+ * @return The procedure returns TRUE if successful, and FALSE if it failed 
+ *         (Wms_Qli50_Error_Number and Wms_Qli50_Error_String are filled in on failure).
+ * @see #Server_Data
+ */
+int Wms_Qli50_Server_Set_Par_Callback(char *class,char *source,Par_Callback_T callback)
+{
+	Server_Data.Par_Callback = callback;
+	return TRUE;
+}
+
+/**
  * Routine to start the server. In this case, open the connection to the specified serial device.
  * @param class The class parameter for logging.
  * @param source The source parameter for logging.
@@ -108,10 +143,119 @@ int Wms_Qli50_Server_Start(char *class,char *source,char *device_name)
 	return TRUE;
 }
 
+/**
+ * Routine to sit on the open serial port, reading lines of data (commands) terminatred by the terminator,
+ * and based on the command returning a fake reply, or invoking a callback, or printing an error to stderr.
+ * @param class The class parameter for logging.
+ * @param source The source parameter for logging.
+ * @return The procedure returns TRUE if successful, and FALSE if it failed 
+ *         (Wms_Qli50_Error_Number and Wms_Qli50_Error_String are filled in on failure).
+ * @see wms_wxt536_command.html#TERMINATOR_CR
+ * @see wms_wxt536_connection.html#Wms_Qli50_Serial_Handle
+ * @see wms_wxt536_general.html#Wms_Qli50_Log
+ * @see wms_wxt536_general.html#Wms_Qli50_Log_Format
+ * @see ../../serial/cdocs/wms_serial_serial.html#Wms_Serial_Read_Line
+ */
 int Wms_Qli50_Server_Loop(char *class,char *source)
 {
-	int done;
+	char command_message_string[256];
+	char reply_message_string[256];
+	char parameter_string[32];
+	char parameter_char;
+	int done,retval,bytes_read;
 
-	
+	done = FALSE;
+	while(done == FALSE)
+	{
+		retval = Wms_Serial_Read_Line(class,source,Wms_Qli50_Serial_Handle,TERMINATOR_CR,
+					      command_message_string,255,&bytes_read);
+		if(retval)
+		{
+			if(strstr(command_message_string,"CLOSE") != NULL)
+			{
+				sprintf(reply_message_string,"LINE CLOSED%s",TERMINATOR_CR);
+				retval = Wms_Serial_Write(class,source,Wms_Qli50_Serial_Handle,reply_message_string,
+							  strlen(reply_message_string));
+				if(retval == FALSE)
+				{
+					Wms_Serial_Error();
+				}
+			}/* end if command was "CLOSE" */
+			else if(strstr(command_message_string,"ECHO") != NULL)
+			{
+				/* parse parameter */
+				retval = sscanf(command_message_string,"ECHO %31s",parameter_string);
+				if(retval == 1)
+				{
+					/* construct reply */
+					sprintf(reply_message_string,"ECHO %s%s",parameter_string,TERMINATOR_CR);
+					retval = Wms_Serial_Write(class,source,Wms_Qli50_Serial_Handle,reply_message_string,
+								  strlen(reply_message_string));
+					if(retval == FALSE)
+					{
+						Wms_Serial_Error();
+					}
+				}
+				else
+				{
+					Wms_Qli50_Error_Number = 200;
+					sprintf(Wms_Qli50_Error_String,
+						"Wms_Qli50_Server_Loop:Failed to parse ECHO command '%s' (%d).",
+						command_message_string,retval);
+					Wms_Qli50_Error();
+				}
+			}/* end if command was "ECHO" */
+			else if(strstr(command_message_string,"OPEN") != NULL)
+			{
+				/* parse parameter */
+				retval = sscanf(command_message_string,"OPEN %c",&parameter_char);
+				if(retval == 1)
+				{
+					/* construct reply */
+					sprintf(reply_message_string,"OPEN %c%s",parameter_char,TERMINATOR_CR);
+					retval = Wms_Serial_Write(class,source,Wms_Qli50_Serial_Handle,reply_message_string,
+								  strlen(reply_message_string));
+					if(retval == FALSE)
+					{
+						Wms_Serial_Error();
+					}
+				}
+				else
+				{
+					Wms_Qli50_Error_Number = 201;
+					sprintf(Wms_Qli50_Error_String,
+						"Wms_Qli50_Server_Loop:Failed to parse OPEN command '%s' (%d).",
+						command_message_string,retval);
+					Wms_Qli50_Error();
+				}
+			}/* end if command was "OPEN" */
+			if(strstr(command_message_string,"PAR") != NULL)
+			{
+				if(Server_Data.Par_Callback != NULL)
+					Server_Data.Par_Callback(reply_message_string,254);
+				else
+					strcpy(reply_message_string,"PARAMETERS");
+				strcat(reply_message_string,TERMINATOR_CR);
+				retval = Wms_Serial_Write(class,source,Wms_Qli50_Serial_Handle,reply_message_string,
+							  strlen(reply_message_string));
+				if(retval == FALSE)
+				{
+					Wms_Serial_Error();
+				}
+			}/* end if command was "CLOSE" */
+			
+			else /* we don't know what this command is */
+			{
+				Wms_Qli50_Error_Number = 202;
+				sprintf(Wms_Qli50_Error_String,
+					"Wms_Qli50_Server_Loop:Failed to parse unknown command '%s'.",command_message_string);
+				Wms_Qli50_Error();
+			}
+		}/* end if command was read successfully */
+		else
+		{
+			Wms_Serial_Error();
+		}
+	}/* end while */
 	return TRUE;
 }
--- qli50/include/wms_qli50_server.h
+++ qli50/include/wms_qli50_server.h
@@ -8,12 +8,23 @@
 typedef int (*Read_Sensor_Callback_T)(char qli_id,char seq_id);
 
 /**
- * Typedef for a fucntion pointer to be invoked when a Send Results command is received by the server.
+ * Typedef for a function pointer to be invoked when a Send Results command is received by the server.
  */
 typedef int (*Send_Result_Callback_T)(char qli_id,char seq_id,struct Wms_Qli50_Data_Struct *data);
 	
+/**
+ * Typedef for a function pointer to be invoked when a PAR command is received by the server.
+ */
+typedef int (*Par_Callback_T)(char *return_parameter_string,int return_parameter_string_length);
+/**
+ * Typedef for a function pointer to be invoked when a STA (status) command is received by the server.
+ */
+typedef int (*Sta_Callback_T)(char *return_string,int return_string_length);
+	
 extern int Wms_Qli50_Server_Set_Read_Sensor_Callback(char *class,char *source,Read_Sensor_Callback_T callback);
 extern int Wms_Qli50_Server_Set_Send_Result_Callback(char *class,char *source,Send_Result_Callback_T callback);
+extern int Wms_Qli50_Server_Set_Par_Callback(char *class,char *source,Par_Callback_T callback);
+extern int Wms_Qli50_Server_Set_Sta_Callback(char *class,char *source,Sta_Callback_T callback);
 extern int Wms_Qli50_Server_Start(char *class,char *source,char *device_name);
 extern int Wms_Qli50_Server_Loop(char *class,char *source);
 
