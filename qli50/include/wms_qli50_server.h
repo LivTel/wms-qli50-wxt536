54f515c3b37ed411ce4e35613d2391a0755edfa8
Chris Mottram <C.Mottram@ljmu.ac.uk>
2021-10-27 16:53:49 +0100

    Changed wms_qli50_server.h declaration of callbacks from int to void.

--- qli50/include/wms_qli50_server.h
+++ qli50/include/wms_qli50_server.h
@@ -9,21 +9,21 @@
 /**
  * Typedef for a function pointer to be invoked when a Read Sensor command is received by the server.
  */
-typedef int (*Read_Sensor_Callback_T)(char qli_id,char seq_id);
+typedef void (*Read_Sensor_Callback_T)(char qli_id,char seq_id);
 
 /**
  * Typedef for a function pointer to be invoked when a Send Results command is received by the server.
  */
-typedef int (*Send_Result_Callback_T)(char qli_id,char seq_id,struct Wms_Qli50_Data_Struct *data);
+typedef void (*Send_Result_Callback_T)(char qli_id,char seq_id,struct Wms_Qli50_Data_Struct *data);
 	
 /**
  * Typedef for a function pointer to be invoked when a PAR command is received by the server.
  */
-typedef int (*Par_Callback_T)(char *return_parameter_string,int return_parameter_string_length);
+typedef void (*Par_Callback_T)(char *return_parameter_string,int return_parameter_string_length);
 /**
  * Typedef for a function pointer to be invoked when a STA (status) command is received by the server.
  */
-typedef int (*Sta_Callback_T)(char *return_string,int return_string_length);
+typedef void (*Sta_Callback_T)(char *return_string,int return_string_length);
 	
 extern int Wms_Qli50_Server_Set_Read_Sensor_Callback(char *class,char *source,Read_Sensor_Callback_T callback);
 extern int Wms_Qli50_Server_Set_Send_Result_Callback(char *class,char *source,Send_Result_Callback_T callback);
